2c234ff0f5bbf7947569ada0228c99d8aa6bfbd8
Ashish Saha <ashish.saha@nxtgensystems.ie>
2026-03-27 11:32:41 +0530

    feat: remove unused routes for record types and investigation options

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 import express from "express";
 import IncidentRouter from "./Incident/incident.route.js";
-import recordTypeRoutes from "./RecordTypes/recordType.route.js";
-import investigationOptionRoutes from "./InvestigationOptions/investigationOption.route.js";
 import adminRouter from "./admin/admin.routes.js";
 import UserRouter from "./user/user.router.js";
 import authRouter from "./auth/auth.routes.js";
@@ -11,8 +9,6 @@ const router = express.Router();
 router.use("/auth", authRouter);
 router.use("/user", UserRouter);
 router.use("/incident", IncidentRouter);
-router.use("/record-types", recordTypeRoutes);
-router.use("/investigation-options", investigationOptionRoutes);
 router.use("/", adminRouter);
 
 export default router;
